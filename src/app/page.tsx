6c832ccf1495e80d55620e9c1bd08d5e9a04efb0
Akashyadav18 <shadowlee5469@gmail.com>
2025-03-01 20:40:02 +0530

    Created and connected with DB

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      Home Page
+    </div>
+  );
+}
